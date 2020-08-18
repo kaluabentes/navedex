891d0cf1b2de05174fead2f407205cfb919f36fb
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 12:34:29 -0400

    feat: create input component

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+import { COLOR_PRIMARY } from "styles/colors";
+
+export default styled.input`
+  font-size: 1rem;
+  padding: 10px 8px;
+  border-radius: 0;
+  width: 100%;
+  outline: none;
+  border: 1px solid ${COLOR_PRIMARY};
+`;
