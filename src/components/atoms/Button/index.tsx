0a53b95af6ca79334b13d48ae5da09bac6f05513
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 12:34:00 -0400

    feat: create button component

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { COLOR_PRIMARY } from "styles/colors";
+
+export default styled.button`
+  font-size: 0.875rem;
+  padding: 12px 8px;
+  background: ${COLOR_PRIMARY};
+  border: 0;
+  color: white;
+  width: 100%;
+  display: block;
+`;
