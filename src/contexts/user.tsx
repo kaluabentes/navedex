33bb44801f0af7f5c0b0977274deb645daf4a272
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 14:01:11 -0400

    feat: create user context

---
+++ src/contexts/user.tsx
@@ -0,0 +1,60 @@
+import React, {
+  createContext,
+  ReactNode,
+  useState,
+  useEffect,
+  useContext,
+} from "react";
+
+interface State {
+  token: string | undefined;
+}
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export type Dispatch = (payload: State) => void;
+
+const INITIAL_STATE = { token: undefined };
+
+const UserStateContext = createContext<State>(INITIAL_STATE);
+const UserDispatchContext = createContext<Dispatch | undefined>(undefined);
+
+export function UserProvider({ children }: UserProviderProps) {
+  const [state, setState] = useState<State>(INITIAL_STATE);
+
+  useEffect(() => {
+    const persistedState = localStorage.getItem("user");
+
+    if (persistedState) {
+      setState(JSON.parse(persistedState));
+    }
+  }, []);
+
+  function dispatch(payload: State) {
+    const nextState = { ...state, ...payload };
+
+    localStorage.setItem("user", JSON.stringify(nextState));
+    setState(nextState);
+  }
+
+  return (
+    <UserStateContext.Provider value={state}>
+      <UserDispatchContext.Provider value={dispatch}>
+        {children}
+      </UserDispatchContext.Provider>
+    </UserStateContext.Provider>
+  );
+}
+
+export function useUserContext(): any {
+  const state = useContext(UserStateContext);
+  const dispatch = useContext(UserDispatchContext);
+
+  if (state === undefined || dispatch === undefined) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
+
+  return [state, dispatch] as const;
+}
