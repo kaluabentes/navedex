5baf9cc1c2973e70613fa8b84a1eb3733d3ac6a7
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 12:35:47 -0400

    feat: create login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 
+import LoginForm from "components/organisms/LoginForm";
+import CenterElementPage from "components/atoms/CenterElementPage";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit() {}
+
   return (
     <>
       <Helmet>
         <title>Login - Navedex</title>
       </Helmet>
-      <h1>Login</h1>
+      <CenterElementPage>
+        <LoginForm
+          email={email}
+          password={password}
+          onEmailChange={(event) => setEmail(event.target.value)}
+          onPasswordChange={(event) => setPassword(event.target.value)}
+          onSubmit={handleSubmit}
+        />
+      </CenterElementPage>
     </>
   );
 }
