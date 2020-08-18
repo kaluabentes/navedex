22e09f78d70a75b0cb86d9535518802cc3cf10ce
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 17:22:30 -0400

    refactor: add title header

--- src/pages/Navers.tsx
+++ src/pages/Navers.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 import App from "components/templates/App";
+import Title from "components/atoms/Title";
+import Button from "components/atoms/Button";
+import TitleHeader from "components/organisms/TitleHeader";
 
 export default function Navers() {
-  return <App>Navers</App>;
+  return (
+    <App>
+      <TitleHeader>
+        <Title size="large">Navers</Title>
+        <Button isInline>Adicionar Naver</Button>
+      </TitleHeader>
+    </App>
+  );
 }
