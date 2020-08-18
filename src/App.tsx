05193b82bd0a2b07937513c2b55ce91ad945f709
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 12:36:28 -0400

    refactor: add font link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Routes from "./Routes";
 import GlobalStyle from "./styles/GlobalStyle";
@@ -7,6 +8,12 @@ import GlobalStyle from "./styles/GlobalStyle";
 export default function App() {
   return (
     <Router>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <GlobalStyle />
       <Routes />
     </Router>
