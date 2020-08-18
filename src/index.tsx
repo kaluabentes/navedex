3aefafa2d8cbd4ea1da734b9a9f9aeacc4fd10f2
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 14:02:06 -0400

    refactor: add hocs

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { UserProvider } from "contexts/user";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
