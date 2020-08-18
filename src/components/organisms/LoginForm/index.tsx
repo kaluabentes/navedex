145ac5f4cca559c3a5127d73ba43cf108d47ff94
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 14:02:20 -0400

    fix: add correct id

--- src/components/organisms/LoginForm/index.tsx
+++ src/components/organisms/LoginForm/index.tsx
@@ -34,7 +34,7 @@ export default function LoginForm({
         placeholder="E-mail"
       />
       <InputLabel
-        id="email"
+        id="password"
         onChange={onPasswordChange}
         value={password}
         label="Senha"
