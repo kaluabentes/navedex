dbdc046d9f8c65adf12c3ccc0904ee2f909abed4
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-19 17:31:13 -0400

    feat: create naver form

---
+++ src/components/organisms/NaverForm/index.tsx
@@ -0,0 +1,128 @@
+import React, { useRef } from "react";
+
+import InputLabel from "components/molecules/InputLabel";
+import Button from "components/atoms/Button";
+import { Form, InputsGrid, ActionContainer } from "./styles";
+
+interface NaveFormProps {
+  isLoading: boolean;
+  name: string;
+  role: string;
+  age: string;
+  companyTime: string;
+  projects: string;
+  image: string;
+  submitLabel: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: () => void;
+}
+
+export default function NaveForm({
+  isLoading,
+  name,
+  role,
+  age,
+  companyTime,
+  projects,
+  image,
+  submitLabel,
+  onChange,
+  onSubmit,
+}: NaveFormProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  function handleSubmit() {
+    const formElement = formRef.current as HTMLFormElement;
+
+    if (!formElement.checkValidity()) {
+      formElement.reportValidity();
+      return;
+    }
+
+    onSubmit();
+  }
+
+  function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  }
+
+  return (
+    <Form ref={formRef}>
+      <InputsGrid>
+        <InputLabel
+          id="name"
+          name="name"
+          type="text"
+          label="Nome"
+          placeholder="Nome"
+          value={name}
+          onChange={onChange}
+          isRequired
+        />
+        <InputLabel
+          id="role"
+          name="role"
+          type="text"
+          label="Cargo"
+          placeholder="Cargo"
+          value={role}
+          onChange={onChange}
+          isRequired
+        />
+        <InputLabel
+          id="age"
+          name="age"
+          type="text"
+          label="Idade"
+          placeholder="Idade"
+          value={age}
+          onChange={onChange}
+          isRequired
+        />
+        <InputLabel
+          id="companyTime"
+          name="companyTime"
+          type="text"
+          label="Tempo de empresa"
+          placeholder="Tempo de empresa"
+          value={companyTime}
+          onChange={onChange}
+          isRequired
+        />
+        <InputLabel
+          id="projects"
+          name="projects"
+          type="text"
+          label="Projetos que participou"
+          placeholder="Projetos que participou"
+          value={projects}
+          onChange={onChange}
+          isRequired
+        />
+        <InputLabel
+          id="image"
+          name="image"
+          type="text"
+          label="URL da foto do Naver"
+          placeholder="URL da foto do Naver"
+          value={image}
+          onChange={onChange}
+          onKeyPress={handleKeyPress}
+          isRequired
+        />
+      </InputsGrid>
+      <ActionContainer>
+        <Button
+          variant="primary"
+          onClick={handleSubmit}
+          isInline
+          isLoading={isLoading}
+        >
+          {submitLabel}
+        </Button>
+      </ActionContainer>
+    </Form>
+  );
+}
---
+++ src/components/organisms/NaverForm/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Form = styled.form``;
+
+export const InputsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 32px;
+  margin-bottom: 32px;
+`;
+
+export const ActionContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
