7932112e4987e2a93892d33960adaf2d3cb73ea4
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 23:05:41 -0400

    refactor: add naver detail modal

--- src/pages/Navers.tsx
+++ src/pages/Navers.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import App from "components/templates/App";
 import Title from "components/atoms/Title";
@@ -8,9 +8,17 @@ import NaverCardGrid from "components/organisms/NaverCardGrid";
 import NaverCard from "components/molecules/NaverCard";
 import useNaversIndex from "hooks/useNaversIndex";
 import { Naver } from "services/NaversApi";
+import NaverDetailModal from "components/organisms/NaverDetailModal";
 
 export default function Navers() {
   const [navers, isLoading] = useNaversIndex();
+  const [naver, setNaver] = useState<Naver | undefined>(undefined);
+  const [isNaverOpen, setIsNaverOpen] = useState(false);
+
+  function handleNaverClick(naver: Naver) {
+    setNaver(naver);
+    setIsNaverOpen(true);
+  }
 
   return (
     <App>
@@ -33,12 +41,20 @@ export default function Navers() {
               image={naver.image}
               name={naver.name}
               role={naver.role}
+              onClick={() => handleNaverClick(naver)}
               onDelete={() => {}}
               onEdit={() => {}}
             />
           ))
         )}
       </NaverCardGrid>
+      <NaverDetailModal
+        isOpen={isNaverOpen}
+        naver={naver || {}}
+        onClose={() => setIsNaverOpen(false)}
+        onDelete={() => {}}
+        onEdit={() => {}}
+      />
     </App>
   );
 }
