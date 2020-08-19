9608849991969d4016159ae62a28dc55d17c05a2
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-19 17:31:55 -0400

    feat: add method to create naver

--- src/services/NaversApi.ts
+++ src/services/NaversApi.ts
@@ -10,6 +10,11 @@ export interface Naver {
   projects: string;
 }
 
+function getLocalData() {
+  const persistedData = localStorage.getItem("navers") as string;
+  return JSON.parse(persistedData);
+}
+
 export default class NaversApi {
   static index(): Promise<any> {
     return new Promise((resolve) => {
@@ -30,12 +35,23 @@ export default class NaversApi {
   static delete(id: number): Promise<any> {
     return new Promise((resolve) => {
       setTimeout(() => {
-        const persistedData = localStorage.getItem("navers") as string;
-        const data = JSON.parse(persistedData);
+        const data = getLocalData();
         const newData = data.filter((naver: Naver) => naver.id !== id);
 
         localStorage.setItem("navers", JSON.stringify(newData));
         resolve({ data: { id } });
+      }, 2000);
+    });
+  }
+
+  static store(naver: any): Promise<any> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const data = getLocalData();
+        const newData = [...data, { id: data.length + 1, ...naver }];
+
+        localStorage.setItem("navers", JSON.stringify(newData));
+        resolve({ data: naver });
       }, 2000);
     });
   }
