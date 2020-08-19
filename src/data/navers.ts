92d04c00b3635597f8e294be8982f3d2aab99ad2
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 20:50:57 -0400

    feat: create mock data

---
+++ src/data/navers.ts
@@ -0,0 +1,43 @@
+import naver1 from "./images/naver1.jpg";
+import naver2 from "./images/naver2.jpg";
+import naver3 from "./images/naver3.jpg";
+import naver4 from "./images/naver4.png";
+
+export default [
+  {
+    id: 1,
+    image: naver1,
+    name: "Juliano Reis",
+    role: "Front-end Developer",
+    age: "Lorem Ipsum",
+    companyTime: "Lorem Ipsum",
+    projects: "Lorem Ipsum",
+  },
+  {
+    id: 2,
+    image: naver2,
+    name: "Gabriel do Couto",
+    role: "Front-end Developer",
+    age: "Lorem Ipsum",
+    companyTime: "Lorem Ipsum",
+    projects: "Lorem Ipsum",
+  },
+  {
+    id: 3,
+    image: naver3,
+    name: "Eduardo Bittencourt",
+    role: "Front-end Developer",
+    age: "Lorem Ipsum",
+    companyTime: "Lorem Ipsum",
+    projects: "Lorem Ipsum",
+  },
+  {
+    id: 4,
+    image: naver4,
+    name: "Gustavo Pinho",
+    role: "Technology Manager",
+    age: "Lorem Ipsum",
+    companyTime: "Lorem Ipsum",
+    projects: "Lorem Ipsum",
+  },
+];
