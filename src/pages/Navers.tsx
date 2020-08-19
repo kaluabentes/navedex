f33b169c1997af77f0137adcac2225a4b1320ad2
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 20:51:48 -0400

    refactor: add loaders

--- src/pages/Navers.tsx
+++ src/pages/Navers.tsx
@@ -4,14 +4,41 @@ import App from "components/templates/App";
 import Title from "components/atoms/Title";
 import Button from "components/atoms/Button";
 import TitleHeader from "components/organisms/TitleHeader";
+import NaverCardGrid from "components/organisms/NaverCardGrid";
+import NaverCard from "components/molecules/NaverCard";
+import useNaversIndex from "hooks/useNaversIndex";
+import { Naver } from "services/NaversApi";
 
 export default function Navers() {
+  const [navers, isLoading] = useNaversIndex();
+
   return (
     <App>
       <TitleHeader>
         <Title size="large">Navers</Title>
         <Button isInline>Adicionar Naver</Button>
       </TitleHeader>
+      <NaverCardGrid>
+        {isLoading ? (
+          <>
+            <NaverCard.Loader />
+            <NaverCard.Loader />
+            <NaverCard.Loader />
+            <NaverCard.Loader />
+          </>
+        ) : (
+          navers.map((naver: Naver) => (
+            <NaverCard
+              key={naver.id}
+              image={naver.image}
+              name={naver.name}
+              role={naver.role}
+              onDelete={() => {}}
+              onEdit={() => {}}
+            />
+          ))
+        )}
+      </NaverCardGrid>
     </App>
   );
 }
