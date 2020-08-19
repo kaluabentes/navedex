946a08422cee66e75b8fdbd6ede83767231bda8d
Kaluã Bentes <kaluanbentes@gmail.com>
2020-08-18 23:04:19 -0400

    refactor: add margin bottom prop

--- src/components/atoms/Title/index.tsx
+++ src/components/atoms/Title/index.tsx
@@ -4,19 +4,21 @@ import { LargeTitle, SmallTitle } from "./styles";
 
 const DEFAULT_PROPS = {
   size: "large",
+  marginBottom: 0,
 };
 
 interface TitleProps {
   children: React.ReactNode;
   size?: "small" | "large";
+  marginBottom?: number;
 }
 
-export default function Title({ size, children }: TitleProps) {
+export default function Title({ size, children, marginBottom }: TitleProps) {
   if (size === "small") {
-    return <SmallTitle>{children}</SmallTitle>;
+    return <SmallTitle marginBottom={marginBottom}>{children}</SmallTitle>;
   }
 
-  return <LargeTitle>{children}</LargeTitle>;
+  return <LargeTitle marginBottom={marginBottom}>{children}</LargeTitle>;
 }
 
 Title.defaultProps = DEFAULT_PROPS;
--- src/components/atoms/Title/styles.ts
+++ src/components/atoms/Title/styles.ts
@@ -1,9 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const LargeTitle = styled.h2`
+export const LargeTitle = styled.h2<{ marginBottom?: number }>`
   font-size: 2.5rem;
   font-weight: 600;
   margin: 0;
+
+  ${(props) => css`
+    margin-bottom: ${props.marginBottom}px;
+  `}
 `;
 
-export const SmallTitle = styled.h3``;
+export const SmallTitle = styled.h3<{ marginBottom?: number }>`
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin: 0;
+
+  ${(props) => css`
+    margin-bottom: ${props.marginBottom}px;
+  `}
+`;
